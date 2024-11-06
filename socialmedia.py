ad85ce1e88b9a222f48b5882bcb825ff1dfc29e1
Ruben <rubenfds@icloud.com>
2024-11-06 21:30:18 +0200

    Added main class with functions

--- socialmedia.py
+++ socialmedia.py
@@ -1,4 +1,31 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class ConnectionsManager:
+    def __init__(self):
+        self.graph = nx.Graph()
+
+    def addUser(self, username):
+        self.graph.addNode(username)
+        print(f'User {username} added.')
+
+    def addConnection(self, user1, user2):
+        self.graph.addEdge(user1, user2)
+        print(f'Connection added between {user1} and {user2}.')
+
+    def viewUsers(self):
+        print("All users:", list(self.graph.nodes))
+
+    def viewConnections(self):
+        print("Connections:", list(self.graph.edges))
+
+    def displayGraph(self):
+        nx.draw(self.graph, with_labels=True)
+        plt.show()
+
 def main():
+    manager = ConnectionsManager()
+    
     while True:
         print("\n1. Add a user")
         print("2. Add a connection between users")
@@ -11,19 +38,22 @@ def main():
         print("\n")
         
         if choice == "1":
-            pass
+            username = input("Enter a username: ")
+            manager.addUser(username)
         
         elif choice == "2":
-            pass
+            user1 = input("Enter the first username: ")
+            user2 = input("Enter the second username: ")
+            manager.addConnection(user1,user2)
         
         elif choice == "3":
-            pass
+            manager.viewUsers()
         
         elif choice == "4":
-            pass
+            manager.viewConnections()
         
         elif choice == "5":
-            pass
+            manager.displayGraph()
         
         elif choice == '6':
             print("\nCheers!\n")
