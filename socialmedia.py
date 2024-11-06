4f773a9960f68de908d29e270dcfac9393570c3b
Ruben <rubenfds@icloud.com>
2024-11-06 21:17:03 +0200

    Added the main menu

---
+++ socialmedia.py
@@ -0,0 +1,29 @@
+def main():
+    while True:
+        print("\n1. Add a user")
+        print("2. Add a connection between users")
+        print("3. View all current users")
+        print("4. View all current connections")
+        print("5. Display connections graph")
+        print("6. Exit the programme")
+        
+        choice = input("Select one of the optioins: ")
+        print("\n")
+        
+        if choice == "1":
+            pass
+        
+        if choice == "2":
+            pass
+        
+        if choice == "3":
+            pass
+        
+        if choice == "4":
+            pass
+        
+        if choice == "5":
+            pass
+        
+        if choice == "6":
+            pass
